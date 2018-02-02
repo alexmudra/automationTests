d2b2ab13983504d948642ca274efb83ea0a6105b
alexmudra <alexander.kobko@gmail.com>
2018-02-02 22:07:26 +0200

    createFirstJavaProgramIn2018y

--- src/mypackage/testClass.java
+++ src/mypackage/testClass.java
@@ -3,5 +3,10 @@ package mypackage;
 /**
  * Created by Alex on 2/2/2018.
  */
-public class testClass {
+public class testClass { //название класса должно совпадать с названием внутри package
+
+    public static void main(String[] args){ // этот метод обязателен, является точкой входа в программу
+        //psv + Tab хоткей что бы быстро написать етот метод
+        System.out.println("Hello Alex. You've just created your first Java program in 2018!");
+    }
 }
