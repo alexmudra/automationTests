e666271653c517d78c6ae8b9fd33005e38ce0f29
alexmudra <alexander.kobko@gmail.com>
2018-02-03 14:27:48 +0200

    overviewAboutArrays

--- src/mypackage/testClass.java
+++ src/mypackage/testClass.java
@@ -7,53 +7,57 @@ public class testClass { //название класса должно совпа
 
     public static void main(String[] args){ // этот метод обязателен, является точкой входа в программу
 
-        /*НАЗВАНИЕ ПЕРЕМЕННЫХ ДОЛЖНО БЫТЬ ЛОГИЧНЫМ, НАЧИНАТЬСЯ С БОЛЬШОЙ БУКВЫ(ЕСЛИ ОДНА БУКВА)
-        ИЛИ ПЕРВОЕ СЛОВО С МАЛЕНЬКОЙ И ПОСЛЕДУЮЩИЕ СЛОВА НАЧИНАЮТЬСЯ С БОЛЬШОЙ БУКВЫ*/
+        //МАСИВЫ ARRAYS
+
+        String [] names; // создаем масив с названием names
+        names = new String[3]; /*выделили область памяти для хранения елементов типа String и размер даного масива будет
+        3 елемента*/
+
+        String[] names2 = new String[3]; // создали такой же масив только код короче
 
 
-        byte a = 5; /*создали переменную с типом byte назвали а и присвоили значение 5 (переменная типа
-        byte может хранить в себе целые числа от -128 до 127)*/
-        short b = -100; //-32768 до 32767 целочисленный тип
-        char e = '$'; /*беззнаковое целое число, представляющее собой символ UTF-16 (буквы и цифры)
-        В UTF-16 символы кодируются двухбайтовыми словами с использованием всех возможных диапазонов значений
-         (от 0 до FFFF16)*/
-        int c = 1000000; // целочисленный тип и самый популярный от -2147483648 до 2147483647
-        long d = 1234398374; //т -9223372036854775808 до 9223372036854775807
+//создали масив с 10 елементов типа integer(число)
 
-        /*System.out.println("byte a:  " + a);
-        System.out.println("short b:  " + b);
-        System.out.println("char e:  " + e);
-        System.out.println("int c:  " + c);
-        System.out.println("long d:  " + d);*/
+        int[] nameOfintArray = new int[10];
 
-        //ТИПЫ ВЕЩЕСТВЕННЫХ ЧИСЕЛ
+        // ЗАПОЛНЯЕМ КАЖДУЮ ЯЧЕЙКУ МАСИВА names2 строковое значение
 
-        float float1 = 123.50f; /* число с плавающей точкой
-        позволяет хранить от -1.4e-45f до 3.4e+38f не забываем ставить f после присвоения значения*/
-        
-        System.out.println("This is float value: " + float1);
+        names2[0] = "Alex";
+        names2[1] = "Roma";
+        names2[2] = "Illia";
 
-        double double1 = 123.32; // позв. хранить дроби от -4.9e-324 до 1.7e+308
-        System.out.println("This is double value: " + double1);
+        System.out.println(names2[2]);
+        System.out.println(names2[1]);
+        System.out.println(names2[0]);
 
-        // Логические типы - Булеан
+        // Пример короткого синтаксиса для создания и заполнения масива
 
-        boolean booleanfirst = true; // инициализировани, объявили и присвоили значение true
-        boolean booleansecond = false;
-        System.out.println("The value of booleangirst is equal " + booleanfirst);
-        System.out.println("The value of booleansecond is equal  "+ booleansecond);
+        int[] array = {12, 13, 123, 2123, 532};
+        System.out.println(array[2]);
 
-        boolean compare1 = 1+2>0;
-        boolean compare2 = 1+2>5;
-        System.out.println("The value of compare1 is equal "+ compare1);
-        System.out.println("The value of compare2 is equal "+ compare2);
+        // Пример вывода всех значений цикла через цикл for
 
-        // ТИП ДАННЫХ STRING
+        for (int i=0; i<5; i++){
+            System.out.println(array[i]); // выветутся на екран все елементы с 0 по 4
+        }
+        // Пример создания и заполнения масива через цыкл
 
-        String str = "Wow,";
-        String str2 = " you are ";
-        String str3 = "genius!";
-        System.out.println("The value of each variables is " +str+str2+str3);
+        int[] createArrayViaFor = new int[11]; // инициализируем новый масив без значений
+
+        for (int j=0; j<11; j++) { // создаем счетчик j
+            createArrayViaFor[j] = j * 10; //присвоим итому елементу j в масиве createArrayViaFor значение j * 10;
+            System.out.println(createArrayViaFor[j]); // виводим результат на екран
+        }
+        System.out.println();
+
+        // Пример вывода на екран значения масива, к-ва елементов
+
+        System.out.println(createArrayViaFor.length);
+
+        // Пример как узнать значение в определенной области памяти масива
+        System.out.println();
+        System.out.println(createArrayViaFor[createArrayViaFor.length-1]); /* выводим значение на екранмы просто отнимаем 1
+        от общего числа индексов масива */
 
     }
 
