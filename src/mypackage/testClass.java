ae19d64c1d08074d7e412def775c7da25af0fffe
alexmudra <alexander.kobko@gmail.com>
2018-02-03 21:47:09 +0200

    introConditionalStatementIfElseSwitch

--- src/mypackage/testClass.java
+++ src/mypackage/testClass.java
@@ -5,123 +5,78 @@ package mypackage;
  */
 public class testClass { //название класса должно совпадать с названием внутри package и начинаться с маленькой буквы
 
-    public static void main(String[] args){ // этот метод обязателен, является точкой входа в программу
+    public static void main(String[] args) { // этот метод обязателен, является точкой входа в программу
 
-        //РАБОТА СО СТРОКАМИ String
+        //УСЛОВНЫЕ ОПЕРАТОРЫ
 
-        String something = "Operations with strings";
+        // Примеры с if  else
 
-        /* Метод .equals возвращает булеан значение */
+        int i = 10;
 
-        System.out.println(something.equals("Operations with strings") ); // сравниваем строки при помощи метода .equals
-        System.out.println(something.equals("Hello!")); //тут вывод будет false потому что строки не одинаковые
-        System.out.println("--------------------------------------------------------");
+        if (i > 5) { //если условие с if будет true, то команда сработает
+            System.out.println("Great: " + "i > 5");
+            i++; // добавляет 1 (инкремент) к значению
+            System.out.println(i);
+        } else { //если команда будет false то выведет сообщение об ошибке
+            System.out.println("Ohh. Error: i <= 5");
+            i--;
+            System.out.println(i);
+        }
+        System.out.println("------------------------------------------------------------");
 
-        String test2 = "Test";
-        System.out.println(something.equals(test2)); // сравнили строку something со строкой test2. Вывод: false
-        System.out.println("--------------------------------------------------------");
+        // Примеры с if, else if
 
-        // Рассмотрим метод equalsIgnoreCase = сравнение проводится без учета регистра
+        if (i > 5) { //если условие с if будет true, то команда сработает
+            System.out.println("Great: " + "i > 5");
+            i++; // добавляет 1 (инкремент) к значению
+            System.out.println(i);
+        } else if (i < 10 && i > 5) { //если результат будет true то сработает этот код
+            System.out.println("Ohh. Error: i < 10 and i > 5");
+            i--;
+            System.out.println(i);
+        } else {
+            System.out.println("Nothing");
+        }
+        System.out.println("------------------------------------------------------------");
 
-        something = "test";
-        test2 = "TEST";
-        System.out.println(something.equalsIgnoreCase(test2)); //вывод true
-        System.out.println("--------------------------------------------------------");
+        //Вложеные условия. Пример
 
-        // Преобразовываем регистры букв методами toLowerCase, toUpperCase
+        i = 100;
+        if (i > 50){
+            System.out.println("The value i > 50");
+            if (i > 90){
+                System.out.println("The value i > 90");
+            }
+            else {
+                System.out.println("The value i <= 90");
+            }
+        }
+        else {
+            System.out.println("Tha value i <= 50");
+        }
+        System.out.println("------------------------------------------------------------");
 
-        something = "TeXt";
-        System.out.println(something);// выводим оригинальное значение строки
-        System.out.println(something.toLowerCase());// этот метод преобразовывает значение строки в маленьком регистре
-        System.out.println(something.toUpperCase());// этот метод преобразовывает весь текст заглавными буквами
-        System.out.println("--------------------------------------------------------");
+        //Оператор множественного  выбора Switch (он проверяет равенство на конкретное значение) это отличие от if
 
-        // Узнаем значение в строке при помощи метода charAt и indexOf
+        i = 6;
 
-        System.out.println();
-        System.out.println(something.charAt(0)); //из нашего значения TeXt выведет Т
-        System.out.println(something.charAt(1));// выведет е
-        System.out.println(something.charAt(2));// выведет Х
-        System.out.println(something.charAt(3));// выведет t
-        System.out.println("--------------------------------------------------------");
-        System.out.println(something.indexOf("X"));// узнаем по значению в строке какой у нее индекс через метод indexOf
-        System.out.println("--------------------------------------------------------");
-
-        // Проверяем содержится ли в строке другая строка методом .contains
-
-        something = "Hello, Alex";
-        System.out.println(something.contains("Alex")); // вывод будет true поскольку в строке есть Alex
-        System.out.println(something.contains("hi")); // вывод false потому что в строке нет hi
-        System.out.println("--------------------------------------------------------");
-
-        // Узнать длинну строки методом .length
-
-        System.out.println(something.length());// строчка "Hello, Alex" содержит в себе 11 символов. Вывод 11
-        System.out.println("--------------------------------------------------------");
-
-        // Проверим начинается (endsWith) или заканчивается (startsWith) строка на какойто символ или значение
-
-        System.out.println(something.startsWith("H"));// будет true
-        System.out.println(something.startsWith("ello")); //false
-        System.out.println(something.endsWith("ex"));// вывод true
-        System.out.println(something.endsWith("Al"));//false
-        System.out.println("--------------------------------------------------------");
-
-        // Разбить строку с помощъю метода .split, (разделитель можна указывать любой)
-
-        something = "Hello, world"; //переопредилили значение строки
-        String[] myArray = something.split(",");
-        System.out.println(myArray[0] + "-" + myArray[1] + "-"); // слова будут разделены -
-        System.out.println(myArray[0]); // первое слово будет в одной строке
-        System.out.println(myArray[1]); //второе слово будет в другой строке
-        System.out.println("--------------------------------------------------------");
-
-        // Форматирование строк с помощью метода .format (пример внизу)
-
-        String str = "My name is %s! I'm %d years old"; /*мы создали шаблон который в последствии можем совместить с
-         некоторыми значениями %s будет в будущем содержать строчное значение, а %d в будущем будет содержать целочисленное значение*/
-        int age = 30;
-        String name = "Alex";
-        System.out.println(String.format(str, name, age));// вывод будет My name is Alex! I'm 30 years old
-        System.out.println(String.format(str, "Vasiliy", 12));// вывод: My name is Vasiliy! I'm 12 years old
-        System.out.println("--------------------------------------------------------");
-
-        // Оперируем числом в строке с помощью метода .parseInt (пример внизу)
-
-        String age2 = "30"; // создаем строку со значением int(целочисленным)
-        int a = Integer.parseInt(age2); //определяем значение строки age2(30) в число с помощью метода parseInt и определяем переменной а
-        System.out.println(a * 10); // вывод 300
-        System.out.println("--------------------------------------------------------");
-
-        // Операция слияния(конкотенация) строк через + или с помощью метода .concat
-
-        // 1 пример с помощью +
-        String first = "I";
-        String second = " am the best";
-        String third = " programmer ever!";
-        String result = first+second+third;
-        System.out.println(result);
-
-        //2 пример с помощью метода .contat Этот метод улучшает производительность системы
-
-        result = first.concat(second).concat(third);
-        System.out.println(result);
-
-        System.out.println("--------------------------------------------------------");
+        switch (i){ //в круглыъ скобках указываем переменную i, которую будем сравнивать с case
+            case 1:
+                System.out.println("The number is 1");
+            case 2:
+                System.out.println("The number is 2");
+            case 3:
+                System.out.println("The number is 3");
+                break; // прерывает проверки(выход из цикла
+            default: //это аналог else в if, если какое то действие не подошло
+                System.out.println("The number is unknown");
+        }
 
 
-        // Вырезаем и выводим часть символов в строке через метод substring (используем индексы
+        System.out.println("------------------------------------------------------------");
 
-        System.out.println(something.substring(1,5));// вырежет ello(4 символы между 1 и 5)
-        System.out.println(something.substring(7)); // можна указать один индекс(7)и все выведется от 7 индекса до конца строки
-        System.out.println("--------------------------------------------------------");
 
-        // Можно совмещать использование методов
-
-        System.out.println(something.substring(7, something.length()-1)); /*вывод будет worl, мы получили часть слова от 7 индекса до
-         предпоследнего символа*/
-        System.out.println("--------------------------------------------------------");
 
     }
-
 }
+
