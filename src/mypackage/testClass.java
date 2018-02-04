daf13343253db8ae38121dbc78c35283ea38438a
alexmudra <alexander.kobko@gmail.com>
2018-02-04 23:23:58 +0200

    reviewSet&PopularMethods

--- src/mypackage/testClass.java
+++ src/mypackage/testClass.java
@@ -1,8 +1,6 @@
 package mypackage;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Alex on 2/2/2018.
@@ -11,46 +9,95 @@ public class testClass { //название класса должно совпа
 
     public static void main(String[] args) { // этот метод обязателен, является точкой входа в программу
 
-        //ИТЕРАТОР - это интерфейс который позволяет работать с елементами списка или колекции
-        // В етом интерфейсе много методов (включая метод .iterator
+        //SET КОЛЕКЦИЯ
 
-        List<Integer> firstList = new ArrayList<>();/*объявляем коллекцию firstList, <указываем типы даных которые будут хранится>
- +        firstList- название колекции, устанавливаем тип листа ArrayList (возможно указать другой тип листа)*/
-            firstList.add(5);//заполняем/добавляем в список с помошью метода .add елемент 5 - етот метод позволяет добавить значение в список
-            firstList.add(10); //добавили в лист значение 10
-            firstList.add(9);
-            firstList.add(11);
-        System.out.println("Values of firstList collection  " +firstList);
+        //Краткое описание разницы между колекциями Lisa and Set
+        /*
+
+╔═══════════════════╦══════════════════════╦═════════════════════════════╗
+║                   ║         List         ║            Set              ║
+║     Duplicates    ║          YES         ║            NO               ║
+╠═══════════════════╬══════════════════════╬═════════════════════════════╣
+║       Order       ║       ORDERED        ║  DEPENDS ON IMPLEMENTATION  ║
+╠═══════════════════╬══════════════════════╬═════════════════════════════╣
+║ Positional Access ║         YES          ║            NO               ║
+╚═══════════════════╩══════════════════════╩═════════════════════════════╝
+        List
+
+        Является упорядоченной группировкой элементов.
+        Список используется для сбора элементов с дубликатами.
+        Новые методы определены внутри списка интерфейс.
+
+        Set
+
+        Является неупорядоченной группировкой элементов.
+        Set используется для сбора элементов без дубликатов.
+        Внутри интерфейса Set не определены новые методы, поэтому мы должны использовать методы интерфейса Collection только с помощью подклассов Set.
+
+        Упорядоченные списки элементов (уникальные или нет)
+
+            Соответствует интерфейсу Java с именем List
+            Доступ к ним можно получить по индексу
+
+            с использованием
+
+            LinkedList
+            ArrayList
+            Списки уникальных элементов:
+            Соответствует интерфейсу Java с именем Set
+            Невозможно получить не по индексу
+
+            с использованием
+
+            HashSet (неупорядоченный)
+            LinkedHashSet (заказывается)
+            TreeSet (отсортированный по естественному порядку или предоставленным компаратором)
+            Оба интерфейса Set и List соответствуют интерфейсу Java с именем Collection
+        */
+
+        Set<Integer> firstSet = new HashSet<>();//создаем новый Set через самую популярную реализацию(HashSet)
+        firstSet.add(10);// добавили елемент 10 через метод .add
+        firstSet.add(11);
+        firstSet.add(11);//значение 11 не запишется в сет (оно не уникальное)
+        firstSet.add(13);
+        firstSet.add(14);
+        System.out.println(firstSet);//выведет только УНИКАЛЬНЫЕ значения(не уникальные даже не попадут в сет)
+
+        firstSet.size();//.size показывает размер сета
+        System.out.println(firstSet.size());
         System.out.println("------------------------------------------------------------------");
 
-        List<Integer> secondList = new ArrayList<>();
-            secondList.add(45);
-            secondList.add(56);
-            secondList.add(66);
-            secondList.add(75);
-            System.out.println("Values of secondList collection  "+ secondList);//размер списка 4
-        System.out.println("------------------------------------------------------------------");
+        //Попробуем работать c Set через итератор(интерфейс). Плюс, используем циклы
 
-        //Создали итератор.
-        //Разбираем метод .next выводит следующий елемент в списке
-        //Разбираем метод .hasnext
-
-        Iterator<Integer> firstIterator = firstList.iterator(); //создали интерфейс с названием firstIterator обращаемся к списку firstList
-        System.out.println(firstIterator.next());//выведет первый (5) елемент списка firstList
-        System.out.println(firstIterator.next());//выведет второй(10) елемент списка firstList
-        System.out.println(firstIterator.next());//выведет третий(9) елемент списка firstList
-        System.out.println(firstIterator.next());//выведет четвертый(11) елемент списка firstList
-        System.out.println(firstIterator.hasNext());//выведет фолс потому что в списке нет 5 елемента(всего 4)
-        System.out.println("------------------------------------------------------------------");
-
-        //Рассмотрим как можно управлять итератором через цикл while
-
-        Iterator<Integer> secondIterator = secondList.iterator();//для примера создали еще один итератор secondIterator
-        while (secondIterator.hasNext()){//цикл будет перебирать все елементы в списке пока метод .hasnext (он есть условия цикла)
-            // будет возвращать тру, и пока не дойдет до последнего елемента в листе
-        System.out.println(secondIterator.next());//метод .next использовали для вывода значений в списке
+        Iterator<Integer> iteratorSet = firstSet.iterator();
+        while (iteratorSet.hasNext()) {
+            System.out.println(iteratorSet.next());// выведет на екран все уникальные значения сета (10,11,13,14)
         }
         System.out.println("------------------------------------------------------------------");
+
+        for (int i : firstSet){
+            System.out.println(i);//выведет на екран все уникальные значения сета (10,11,13,14)
+        }
+        firstSet.remove(14);//с помощью метода .remove удаляем значение, указываем не индекс(как в list),а само
+        //значение которое нужно удалить
+        System.out.println(firstSet);//вывод 10,11,13 (14 был удален)
+        System.out.println("------------------------------------------------------------------");
+
+        /*firstSet.clear();// .clear удаляет все значения с сета
+        System.out.println(firstSet);
+        System.out.println(firstSet.size());
+        System.out.println("------------------------------------------------------------------");*/
+
+        System.out.println(firstSet.contains(10));// true - потому что значение 10 есть в сете
+        System.out.println(firstSet.contains(14));// false - потому что значение 14 есть в сете
+
+        //Примечание! метод .get отсутствует в Set потому, что в Set нет индексов(только уникальные значения)
+
+        System.out.println("------------------------------------------------------------------");
     }
+
+
+
+
 }
 
