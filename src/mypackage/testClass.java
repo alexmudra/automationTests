a10a4c7fa1a07ead30ee0106e1c2bb4d11d0c699
alexmudra <alexander.kobko@gmail.com>
2018-02-14 23:18:38 +0200

    view (classes) introduction to Java Constructors

--- src/mypackage/testClass.java
+++ src/mypackage/testClass.java
@@ -12,6 +12,7 @@ public class testClass { //название класса должно совпа
         // ИЗУЧАЕМ КЛАССЫ НА ПРИМЕРЕ НОВОГО КЛАССА Car (в классе TestClass будем создавать екземпляр метода
 
         Car car1 = new Car();// мы создали обект(car2) класса Car
+        // В круглых скобках мы можем указать параметры для конструктора класса Car
 
         // зададим обекту car1 значения
         car1.color = "Black";
@@ -19,17 +20,28 @@ public class testClass { //название класса должно совпа
         car1.height = 2000;
         car1.width = 2500;
 
-        //вызываем метод с родительского класа car и зададим значение 50
+        // Создадим еще один объект сласса Car с параметром цвета
+        Car car2 = new Car("Grey");
+        System.out.println("Car color is " + car2.color);
+        codeDeviderLine();
 
+        //вызываем метод с родительского класа car и зададим значение 50
         car1.addWeight(50);//результат метода будет: New weight: 2050
+        codeDeviderLine();
 
         car1.drive(120);// результатом метода drive: The speed is Ok. We're ready to drive
+        codeDeviderLine();
 
+        // Создадим новый объект
 
+        Car car3 = new Car("Red", 2, 3, 4);
+        System.out.println("Our car has the following parametrs: "+ car3.color+ " " + car3.width+" " + car3.height+" "+ car3.length);
+        codeDeviderLine();
 
-   // static void codeDeviderLine(){
-     //   System.out.println("---------------------------------"+" End Code "+"-----------------------------------");
     }
-
+    static void codeDeviderLine(){
+        System.out.println("---------------------------------" + " End Code " + "-----------------------------------");
+    }
 }
+
 
