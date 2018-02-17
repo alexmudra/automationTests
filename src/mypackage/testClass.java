ccfa455f382475af11c0fb5490c2eafcd76cece1
alexmudra <alexander.kobko@gmail.com>
2018-02-17 22:28:36 +0200

    view (variables and methods) Static and Final access to Java
    :wq
    :qw
    
    wq
    qw

--- src/mypackage/testClass.java
+++ src/mypackage/testClass.java
@@ -5,13 +5,11 @@ import java.util.*;
 /**
  * Created by Alex on 2/2/2018.
  */
-public class testClass { //название класса должно совпадать с названием внутри package и начинаться с маленькой буквы
+public class testClass { //название класса должно совпадать с названием внутри package и начинаться с большой буквы
 
     public static void main(String[] args) { // этот метод обязателен, является точкой входа в программу
 
-        // ИЗУЧАЕМ КЛАССЫ НА ПРИМЕРЕ НОВОГО КЛАССА Car (в классе TestClass будем создавать екземпляр метода
-
-        Car car1 = new Car();// мы создали обект(car2) класса Car
+              Car car1 = new Car();// мы создали обект(car2) класса Car
         // В круглых скобках мы можем указать параметры для конструктора класса Car
 
         // зададим обекту car1 значения
@@ -37,6 +35,37 @@ public class testClass { //название класса должно совпа
         Car car3 = new Car("Red", 2, 3, 4);
         System.out.println("Our car has the following parametrs: "+ car3.color+ " " + car3.width+" " + car3.height+" "+ car3.length);
         codeDeviderLine();
+
+        /*Для того чтобы обратится с статичной переменной или классу нам можно обратится просто через имя класса.
+        * Тоесть без создания нового екземпляра класса*/
+
+        System.out.println("Static variable is " + Car.var); // мы просто получили значение статичной переменной через имя класса Car
+        // и через точку имя статичной переменной var (Car.var)
+        codeDeviderLine();
+
+        Car.var = 200;// также мы можем изменять значение статичной переменной (было 10) а стало 200
+        System.out.println("Static variable is " + Car.var);
+        codeDeviderLine();
+
+        // Можем вывести значение статического метода через название класса. Н-р: Car.method
+        Car.method(); //результат вывода будет My static method
+        codeDeviderLine();
+
+        // Выводим переменную c модификатором final
+        System.out.println("My final variable is " + Car.final_variable);
+        codeDeviderLine();
+
+        // Выводим метод с модификатором final
+        Car.final_method();
+        codeDeviderLine();
+
+
+
+
+
+
+
+
 
     }
     static void codeDeviderLine(){
